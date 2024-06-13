b68614c86c88b673f298e1ca4e90d2e9f9e747ce
sumimallavolu <sumasrimallavolu@gmail.com>
2024-06-13 20:51:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import EditorPage from "./pages/EditorPage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <EditorPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/EditorPage/index.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import Editor from "@monaco-editor/react";
+import "./styles.css";
+import DownloadImage from "../../assets/icons/download.svg";
+import saveAs from "file-saver";
+
+const files = {
+  js: {
+    name: "script.js",
+    language: "javascript",
+  },
+  css: {
+    name: "style.css",
+    language: "css",
+  },
+  html: {
+    name: "index.html",
+    language: "html",
+  },
+};
+const EditorPage = () => {
+  const [values, setValues] = useState();
+  const [fileName, setFileName] = useState("js");
+  const [isDisabled, setisDisabled] = useState(false);
+  const file = files[fileName];
+
+  const handleCodeChange = (value) => {
+    setValues(value);
+    setisDisabled(false);
+  };
+  const handleSaveClick = () => {
+    try {
+      localStorage.setItem(`${file.name}`, values);
+      window.alert(`code saved in  ${file.name}`);
+      setisDisabled(true);
+    } catch (error) {
+      console.error("Error saving to local storage", error);
+    }
+  };
+  const handleResetClick = () => {
+    try {
+      localStorage.removeItem(`${file.name}`);
+      setValues("");
+    } catch (error) {
+      console.error("Error saving to local storage", error);
+    }
+  };
+
+  const handledownloadFile = (text) => {
+    const blob = new Blob([text], { type: "text/plain" });
+    const link = document.createElement("a");
+    link.download = `${file.name}`;
+    link.href = URL.createObjectURL(blob);
+    document.body.appendChild(link);
+  
+    //  link.onclick();
+    document.body.removeChild(link);
+  };
+
+  useEffect(() => {
+    const items = localStorage.getItem(`${file.name}`);
+    setValues(items);
+  }, [file.name]);
+
+  return (
+    <div>
+      <div className="header"> CODE EDITOR</div>
+      <div className="editor-main">
+        <div>
+          <Editor
+            height="92vh"
+            width="1400px"
+            className=""
+            theme="vs-dark"
+            value={values}
+            path={file.name}
+            defaultLanguage={file.language}
+            defaultValue="// Hello Dev!  Happy Hacking"
+            onChange={handleCodeChange}
+          />
+        </div>
+        <div className="buttons-container">
+          <div onClick={handledownloadFile(values)}>
+            <img src={DownloadImage} className="image" alt="Download" />
+          </div>
+          <div>
+            <button
+              disabled={fileName === "js"}
+              onClick={() => setFileName("js")}
+              className="buttons"
+            >
+              javascript
+            </button>
+          </div>
+          <div>
+            <button
+              className="buttons"
+              disabled={fileName === "css"}
+              onClick={() => setFileName("css")}
+            >
+              CSS
+            </button>
+          </div>
+          <div>
+            <button
+              className="buttons"
+              disabled={fileName === "html"}
+              onClick={() => setFileName("html")}
+            >
+              HTML
+            </button>
+          </div>
+          <div>
+            <button className="buttons" onClick={handleResetClick}>
+              Reset
+            </button>
+          </div>
+          <div>
+            <button
+              disabled={
+                values === localStorage.getItem(`${file.name}`) || isDisabled
+              }
+              className="buttons"
+              onClick={handleSaveClick}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default EditorPage;
